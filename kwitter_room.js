57608b1edf4215b05a794397166e4718e5b7552f
Wshyamthegoat <128105229+Wshyamthegoat@users.noreply.github.com>
2023-03-16 20:44:19 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -8,7 +8,13 @@ const firebaseConfig = {
     appId: "1:442098379928:web:2329f64d4dbcce0fc274ed"
   };
   const app = initializeApp(firebaseConfig);
-
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+    Room_names = childKey;
+   console.log("Room name -", Room_names);
+   row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+   document.getElementById("output").innerHTML+=row;
+   });});}
+getData();
 function addRoom()
 {
 room_name = document.getElementById("room_name").value;
@@ -16,7 +22,7 @@ firebase.database().ref("/").child(room_name).update({
       purpose : "adding room name"
 });
 localStorage.setItem("room_name", room_name);
-window.location = "kwitter_page.html";
+window.location = "Kwitter_page.html";
 }
 function redirectToRoomName(name){
       console.log(name);
